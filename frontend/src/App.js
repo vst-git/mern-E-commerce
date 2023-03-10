82cb9072e1e3605e98a6600a0c6b1bc60673aa5d
Vivek <viveksingh10730@gmail.com>
2023-03-10 11:23:44 +0530

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+      <a href="\">Bucket+.com</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
